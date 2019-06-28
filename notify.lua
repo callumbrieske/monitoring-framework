070e65c92918bcc0d16ea41be9e2fff389218b25
Callum Brieske <38851394+callumbrieske@users.noreply.github.com>
2019-06-28 23:03:32 +1200

    Register control parameters, & json encode.

--- notify.lua
+++ notify.lua
@@ -1,3 +1,5 @@
+json = require("rapidjson")
+
 _NOTIFY = {
 
   _STARTUPBUFFER = {},
@@ -25,19 +27,43 @@ _NOTIFY = {
   
   _UPDATE = function(self)
     -- Update the remote controls.
+    
+    local function registerControl(table, ctl)
+      local params = {"Value", "String", "Color", "Legend"}
+      for i, v in ipairs(params) do
+        _NOTIFY._DIRTY = table[v] ~= ctl[v] or _NOTIFY._DIRTY
+        table[v] = ctl[v]
+      end
+    end
+    
+    for ctlName, ctl in pairs(Controls) do
+      if type(ctl) == "table" then
+        if type(_NOTIFY._CONTROLDATA[ctlName]) ~= "table" then _NOTIFY._CONTROLDATA[ctlName] = {} end
+        for ctlIndex, ctlM in ipairs(ctl) do
+          if type(_NOTIFY._CONTROLDATA[ctlName][ctlIndex]) ~= "table" then _NOTIFY._CONTROLDATA[ctlName][ctlIndex] = {} end
+          registerControl(_NOTIFY._CONTROLDATA[ctlName][ctlIndex], ctlM)
+        end
+      else
+        if type(_NOTIFY._CONTROLDATA[ctlName]) ~= "table" then _NOTIFY._CONTROLDATA[ctlName] = {} end
+        registerControl(_NOTIFY._CONTROLDATA[ctlName], ctl)
+      end
+    end
+    
     if _NOTIFY._DIRTY or _NOTIFY._COUNTSINCEUPDATE >= 10 then
-      print("Tick")
+      print(json.encode(_NOTIFY._CONTROLDATA))
       
       _NOTIFY._COUNTSINCEUPDATE = 0
     end
     
     _NOTIFY._COUNTSINCEUPDATE = _NOTIFY._COUNTSINCEUPDATE + 1
     _NOTIFY._DIRTY = false
+    _NOTIFY._TIMER:Start(1)
   end,
   
   _BEGIN = function(self)
     self._TIMER.EventHandler = self._RUN
-    self._TIMER:Start(1)
+    math.randomseed(os.clock())
+    self._TIMER:Start(math.random(1))
   end,
   
 }
